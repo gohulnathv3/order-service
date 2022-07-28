c8ddface1d08d4d196abd06f924fd357a843b489
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-07-28 08:28:36 +0530

    Added logic in service class and also going to make a rest api call. Will see how to make a rest api call in next commit

--- src/main/java/com/learning/os/api/service/OrderService.java
+++ src/main/java/com/learning/os/api/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.learning.os.api.service;
 
+import com.learning.os.api.common.Payment;
+import com.learning.os.api.common.TransactionRequest;
 import com.learning.os.api.entity.Order;
 import com.learning.os.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,12 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
-    public Order saveOrder(Order order){
+    public Order saveOrder(TransactionRequest request){
+        Order order = request.getOrder();
+        Payment payment = request.getPayment();
+        payment.setOrderId(order.getId());
+        payment.setAmount(order.getPrice());
+        // to make a rest api call for fetching to payment-service
         return repository.save(order);
     }
 }
