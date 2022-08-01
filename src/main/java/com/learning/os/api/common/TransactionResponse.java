be7393faeff5e42d40dcb642943f124f5d6eef68
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-01 22:34:52 +0530

    Update TransactionResponse.java
    Used for reference purpose

--- src/main/java/com/learning/os/api/common/TransactionResponse.java
+++ src/main/java/com/learning/os/api/common/TransactionResponse.java
@@ -1,9 +1,16 @@
 package com.learning.os.api.common;
 
 import com.learning.os.api.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionResponse {
     private Order order;
-    private int amount;
+    private double amount;
     private String transactionId;
+    private String message;
 }
