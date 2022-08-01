3bd7736b05f9bc9b812a4d8279ca04609c32e849
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-01 22:33:41 +0530

    Create SequenceGeneratorService.java
    - Added sequenceGernerator for auto generating the id in mongodb

---
+++ src/main/java/com/learning/os/api/RepoService/SequenceGeneratorService.java
@@ -0,0 +1,30 @@
+package com.learning.os.api.RepoService;
+
+import com.learning.os.api.entity.DbSequence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+import static org.springframework.data.mongodb.core.FindAndReplaceOptions.options;
+
+@Service
+public class SequenceGeneratorService {
+    @Autowired
+    private MongoOperations mongoOperations;
+
+    public int getSequenceNumber(String sequenceName){
+        org.springframework.data.mongodb.repository.Query query = new Query(Criteria.where("id").is(sequenceName));
+        Update update = new Update().inc("seq",1);
+
+        DbSequence counter = mongoOperations.findAndModify(
+                query,update, options().returnNew(true).upsert(true),
+                DbSequence.class);
+        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+        )
+    }
+}
