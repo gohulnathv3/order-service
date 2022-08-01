302796d178709a2c142c9778ee0d252d592fa4bc
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-01 22:35:17 +0530

    Create DbSequence.java

---
+++ src/main/java/com/learning/os/api/entity/DbSequence.java
@@ -0,0 +1,18 @@
+package com.learning.os.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "DbSequence")
+public class DbSequence {
+    @Id
+    private String id;
+    private int seq;
+}
