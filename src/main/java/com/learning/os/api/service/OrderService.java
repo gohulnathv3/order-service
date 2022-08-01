bc5174e8acdc498288067d9f4d636826e86ccf82
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-01 22:34:15 +0530

    Update OrderService.java
    Including rest template and all

--- src/main/java/com/learning/os/api/service/OrderService.java
+++ src/main/java/com/learning/os/api/service/OrderService.java
@@ -2,10 +2,12 @@ package com.learning.os.api.service;
 
 import com.learning.os.api.common.Payment;
 import com.learning.os.api.common.TransactionRequest;
+import com.learning.os.api.common.TransactionResponse;
 import com.learning.os.api.entity.Order;
 import com.learning.os.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -13,12 +15,19 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
-    public Order saveOrder(TransactionRequest request){
+    // Injecting rest template
+    @Autowired
+    private RestTemplate template;
+    public TransactionResponse saveOrder(TransactionRequest request){
+        String response = "";
         Order order = request.getOrder();
         Payment payment = request.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         // to make a rest api call for fetching to payment-service
-        return repository.save(order);
+        Payment paymentResponse = template.postForObject("http://localhost:9191/payment/doPayment",payment,Payment.class);
+        response = paymentResponse.getPaymentStatus().equals("success")?"Payment processed successfully and Order placed:)" : "Payment Failure in paymentApi";
+        repository.save(order);
+        return new TransactionResponse(order, paymentResponse.getAmount(),paymentResponse.getTransactionId(),response);
     }
 }
