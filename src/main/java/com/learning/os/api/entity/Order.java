985b2a27bc299e76538ee609790c595eb5c694fb
Gohul Nath V <64803321+gohulnathv3@users.noreply.github.com>
2022-08-01 22:35:20 +0530

    Update Order.java

--- src/main/java/com/learning/os/api/entity/Order.java
+++ src/main/java/com/learning/os/api/entity/Order.java
@@ -3,18 +3,15 @@ package com.learning.os.api.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "ORDER_TB")
+@Document(collection = "order")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
-    @Id
+
     private int id;
     private String name;
     private int qty;
